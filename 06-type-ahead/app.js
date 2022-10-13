fca632a8a91dbb717e23a7f06a51cc808d9633af
Ciro <cbjales@gmail.com>
2022-10-13 19:02:43 -0300

    chore: add functions for fetching/matching data

--- 06-type-ahead/app.js
+++ 06-type-ahead/app.js
@@ -1,1 +1,18 @@
 const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
+
+const cities = [];
+
+async function fetchData() {
+  let response = await fetch(endpoint);
+  let data = await response.json();
+  cities.push(...data);
+};
+
+fetchData();
+
+function findMatches(word, cities) {
+  let regex = new RegExp(word, "gi");
+  return cities.filter(
+    place => regex.test(place.city) || regex.test(place.state)  
+    );
+}
